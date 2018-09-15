7283b13dc54a918501bf12d3ec9dfbecfceb38e3
Calvin,Xu <csxucaixu@gmail.com>
2018-09-15 23:54:17 +0800

    更新.

--- awk_Learning/splitfile.sh
+++ awk_Learning/splitfile.sh
@@ -3,8 +3,9 @@
 # Author: Calvin,Xu
 # sh splitfile.sh buyingData
 
-# 以Tab为分隔符读入数据，同时传入输入的外部文件名变量
-awk -F'\t' -v filename="$1" \
+filename="output"
+# 以Tab为分隔符读入数据，同时传入外部shell变量
+awk -F'\t' -v outputname="$filename" \
 'BEGIN{OFS="\t"}   # 以tab为分隔符输出数据
 {
     # 标题行不处理，直接读取下一行
@@ -21,5 +22,5 @@ awk -F'\t' -v filename="$1" \
 
     # 此处重定向不同于shell的重定向。在这里首次输出会清空文件，后续是增加。
     # 详细说明见官方文档: https://www.gnu.org/software/gawk/manual/html_node/Redirection.html
-    print user, merchant, product, buyingTime > filename"_"buyingTime;
+    print user, merchant, product, buyingTime > outputname"_"buyingTime;
 }' $1
